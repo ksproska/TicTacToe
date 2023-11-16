3975d5f19966fe6501b9f19d5fbde7c59e9b20f3
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-16 22:04:25 +0100

    simple user endpoint created

---
+++ backend/tictactoe/src/main/java/com/tictactoe/tictactoe/UserController.java
@@ -0,0 +1,16 @@
+package com.tictactoe.tictactoe;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @GetMapping("/list-users")
+    @ResponseBody
+    public List<User> listUsers() {
+        return List.of(new User[]{new User("Kamila", "pass")});
+    }
+}
