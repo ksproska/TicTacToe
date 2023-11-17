6d89a99335357feb0f9d37ab4e95c163a2761868
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-17 11:38:49 +0100

    slight gui improvements

--- frontend/tic-tac-toe/src/app/app.component.ts
+++ frontend/tic-tac-toe/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 
@@ -11,5 +11,5 @@ import {RouterOutlet} from '@angular/router';
 })
 
 export class AppComponent {
-  title = 'tic-tac-toe';
+  title = 'TIC-TAC-TOE';
 }
