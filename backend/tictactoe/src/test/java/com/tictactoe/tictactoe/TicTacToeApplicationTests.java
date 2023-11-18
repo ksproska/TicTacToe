02a4e1f87ed2d7109b6be70e8bed0888c7747869
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-18 11:26:35 +0100

    fixing tests

--- backend/tictactoe/src/test/java/com/tictactoe/tictactoe/TicTacToeApplicationTests.java
+++ backend/tictactoe/src/test/java/com/tictactoe/tictactoe/TicTacToeApplicationTests.java
@@ -2,8 +2,10 @@ package com.tictactoe.tictactoe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TicTacToeApplicationTests {
 
 	@Test
