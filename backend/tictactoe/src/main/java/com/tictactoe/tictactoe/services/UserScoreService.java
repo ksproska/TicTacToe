b137085f25995622e10f195f8cb742f3f9afb7c6
Kamila Sproska <kamila.sproska@gmail.com>
2023-11-22 17:14:43 +0100

    fix for displaying user scores

--- backend/tictactoe/src/main/java/com/tictactoe/tictactoe/services/UserScoreService.java
+++ backend/tictactoe/src/main/java/com/tictactoe/tictactoe/services/UserScoreService.java
@@ -1,45 +1,43 @@
 package com.tictactoe.tictactoe.services;
 
-import com.tictactoe.tictactoe.models.entities.GameScore;
-import com.tictactoe.tictactoe.models.entities.User;
 import com.tictactoe.tictactoe.models.UserScoreResponse;
+import com.tictactoe.tictactoe.models.entities.GameScore;
 import com.tictactoe.tictactoe.repositories.GameScoreRepository;
+import com.tictactoe.tictactoe.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 @Service
 public class UserScoreService {
     private final GameScoreRepository gameScoreRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserScoreService(GameScoreRepository gameScoreRepository) {
+    public UserScoreService(GameScoreRepository gameScoreRepository, UserRepository userRepository) {
         this.gameScoreRepository = gameScoreRepository;
+        this.userRepository = userRepository;
     }
 
     public List<UserScoreResponse> listUsersScores() {
-        return gameScoreRepository
+        var allWinners = gameScoreRepository
                 .findAll()
                 .stream()
                 .map(GameScore::getWinner)
                 .filter(Objects::nonNull)
-                .map(User::getUsername)
-                .reduce(new HashMap<String, Integer>(), (hashMap, e) -> {
-                            hashMap.merge(e, 1, Integer::sum);
-                            return hashMap;
-                        },
-                        (m, m2) -> {
-                            m.putAll(m2);
-                            return m;
-                        }
-                )
-                .entrySet()
+                .toList();
+        return userRepository
+                .findAll()
                 .stream()
-                .map(x -> new UserScoreResponse(x.getKey(), x.getValue()))
+                .map(
+                        user -> new UserScoreResponse(
+                                user.getUsername(),
+                                (int) allWinners.stream().filter(u -> u.equals(user)).count()
+                        )
+                )
                 .sorted(Comparator.comparing(UserScoreResponse::score).reversed())
                 .toList();
     }
