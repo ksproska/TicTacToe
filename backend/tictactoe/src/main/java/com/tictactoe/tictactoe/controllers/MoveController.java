ab68de13e4c5c3ebf4e7952b7c2b8e0f02bde5f3
Kamila Sproska <kamila.sproska@gmail.com>
2023-12-23 18:27:06 +0100

    removed smart session ending (not working)

--- backend/tictactoe/src/main/java/com/tictactoe/tictactoe/controllers/MoveController.java
+++ backend/tictactoe/src/main/java/com/tictactoe/tictactoe/controllers/MoveController.java
@@ -1,21 +1,14 @@
 package com.tictactoe.tictactoe.controllers;
 
 import com.tictactoe.tictactoe.models.MoveRequest;
-import com.tictactoe.tictactoe.models.ValidatedMoveResponse;
 import com.tictactoe.tictactoe.services.GameScoreService;
 import com.tictactoe.tictactoe.services.GameService;
 import jakarta.transaction.Transactional;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.socket.messaging.SessionSubscribeEvent;
-import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
-
-import java.util.*;
 
 @CrossOrigin(origins = "${app.api.settings.cross-origin.urls}")
 @Controller
@@ -23,13 +16,11 @@ public class MoveController {
     private final GameService gameService;
     private final GameScoreService gameScoreService;
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private final Map<String, String> sessions;
 
     public MoveController(GameService gameService, GameScoreService gameScoreService, SimpMessagingTemplate simpMessagingTemplate) {
         this.gameService = gameService;
         this.gameScoreService = gameScoreService;
         this.simpMessagingTemplate = simpMessagingTemplate;
-        this.sessions = new HashMap<>();
     }
 
     @MessageMapping("/requested-move/{gameId}")
@@ -42,41 +33,5 @@ public class MoveController {
                 .getGameFinalScore(game)
                 .ifPresent(gameScoreService::saveGameScore);
         this.simpMessagingTemplate.convertAndSend("/topic/verified-move/" + gameId, validatedMoveResponse);
-    }
-
-    @EventListener
-    public void handleSubscribe(SessionSubscribeEvent event) {
-        var headers = getNativeHeaders(event.getMessage());
-        var id = getId(headers, "id");
-        var destination = getId(headers, "destination");
-        sessions.put(id, destination);
-    }
-
-    @EventListener
-    @Transactional
-    public void handleUnsubscribe(SessionUnsubscribeEvent event) {
-        var headers = getNativeHeaders(event.getMessage());
-        var id = getId(headers, "id");
-        var destination = sessions.get(id);
-        var gameId = Arrays.stream(destination.split("/"))
-                .reduce((first, second) -> second)
-                .map(Long::parseLong)
-                .orElseThrow();
-        this.gameService.setGameFinishedWithoutWinner(gameId);
-        this.simpMessagingTemplate.convertAndSend(destination, ValidatedMoveResponse.GAME_ABANDONED_RESPONSE);
-    }
-
-    private static <T, U> String getId(Map<T, U> headers, String id) {
-        return Optional.ofNullable(headers.get(id))
-                .map(List.class::cast)
-                .map(list -> list.get(0))
-                .map(String.class::cast)
-                .orElseThrow();
-    }
-
-    private static Map getNativeHeaders(Message<byte[]> event) {
-        return Optional.ofNullable(event.getHeaders().get("nativeHeaders"))
-                .map(Map.class::cast)
-                .orElseThrow();
     }
 }
